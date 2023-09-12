c3bcb56d10d77541ee4280f4fce44384eeced285
luborgese <lcborgese@gmail.com>
2023-09-11 23:19:33 -0300

    primeiro commit

---
+++ javascript.js
@@ -0,0 +1,8 @@
+var n = parseInt (prompt ('Digite um numero inteiro: '));
+
+if (n % 2===0 && n > 0 && n < 100) {
+    alert ('O numero atende aos critérios');
+}
+else {
+    alert('O numero não atende aos critérios');
+}
